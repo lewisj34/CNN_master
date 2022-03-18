dd63046ed3b1f3cbbfeab7aab1962b88e18f66e4
civdex <dex.civ@gmail.com>
2022-03-17 23:17:24 -0500

    updated z.py

--- z.py
+++ z.py
@@ -1,8 +1,24 @@
-import torch 
-if __name__ == '__main__':
-    num_gpu = torch.cuda.device_count()
-    if num_gpu > 1:
-        model = torch.nn.DataParallel(model)
-        print("make DataParallel")
-    else:
-        print(f'Number of GPUs avaialble: {num_gpu}')
+"""
+GOAL OF THIS FILE IS TWO-FOLD:
+    1. Modify input so that we can have transforms variable 
+    2. see if we can do something about incoporating the ground truth earlier
+        in network 
+"""
+
+def main():
+    # import data 
+    train_loader = get_dataset(
+        dataset, 
+        save_dir + "/data_train.npy", # location of train images.npy file (str)
+        save_dir + "/mask_train.npy" , # location of train masks.npy file (str)
+        batchsize=batch_size, 
+        normalization="deit" if "deit" in backbone else "vit")
+
+
+
+    val_loader = 
+
+    test_loader = 
+
+if __name__ == "__main__":
+    main()

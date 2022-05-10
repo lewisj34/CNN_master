d6f6fade0e8d2d80ac51609288e2e1517c28645f
civdex <dex.civ@gmail.com>
2022-05-10 18:33:23 -0500

    added requirements.txt and other stuff for NewFusion

--- zzz.py
+++ zzz.py
@@ -1,24 +1,33 @@
-import torch 
+import torch
+import torch.nn as nn 
 import numpy as np 
 
-from seg.utils.check_parameters import count_parameters
+
+class DilatedCNN(nn.Module):
+  def __init__(self):
+    super(DilatedCNN,self).__init__()
+    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 9, stride = 1, padding = 0, dilation=2)
+    self.relu1 = nn.ReLU()
+    self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size = 3, stride = 1, padding= 0, dilation = 2)
+    self.relu2 = nn.ReLU()
+    
+    self.fclayers = nn.Sequential(
+      nn.Linear(2304,120),
+      nn.ReLU(),
+      nn.Linear(120,84),
+      nn.ReLU(),
+      nn.Linear(84,10)
+    )
+  def forward(self, x: torch.Tensor):
+    x = self.conv1(x); print(f'x: {x.shape})')
+    x = self.relu1(x); print(f'x: {x.shape})')
+    x = self.conv2(x); print(f'x: {x.shape})')
+    x = self.relu2(x); print(f'x: {x.shape})')
+    x = x.view(-1,2304)
+    x = self.fclayers(x)
+    return x
 
 if __name__ == '__main__':
-    from seg.model.CNN.CNN import CNN_BRANCH
-    from seg.model.zed.zedNet import zedNet
-    count_parameters(
-        CNN_BRANCH(
-            n_channels=3,
-            n_classes=1, 
-            patch_size=16,
-        )
-    )
-
-    count_parameters(
-        zedNet(
-            n_channels=3,
-            n_classes=1,
-            patch_size=16
-        )
-    )
-
+    x = torch.randn((10, 3, 32, 32), device='cuda')
+    model = DilatedCNN().cuda()
+    out = model(x)

cb4bb07bb4bdfe27bc0e341e79e233d944d7cf39
civdex <dex.civ@gmail.com>
2022-05-26 19:25:41 -0500

    sidInspiredFusionNetwork

--- seg/model/Fusion/sidInspiredFusionNetwork.py
+++ seg/model/Fusion/sidInspiredFusionNetwork.py
@@ -7,19 +7,18 @@ from seg.model.general.count_params_master import count_parameters
 
 from pathlib import Path
 from seg.model.CNN.CNN import CNN_BRANCH
+from seg.model.zed.zedNet import zedNet, zedNetDWSep, zedNetMod
+
 from seg.model.Fusion.CondensedFusion import BNRconv3x3
 from seg.model.alt_cnns.pranetSimple import RFB_modified
 from seg.model.general.DW_sep import SeparableConv2D
 from seg.model.transformer.decoder_new import DecoderMultiClassDilationAndSCSE, DecoderMultiClassDilationAndSCSEFusion, DecoderMultiClassDilationAndSCSEFusionJustOne, DecoderMultiClassDilationAndSCSEReduced, UpModDilatedDWSep, UpModDilated
-from seg.model.transformer.transformerV3 import create_transformerV3
+from seg.model.transformer.transformerNoDecoderAdjustableChannels import create_transformerV5
 
-from seg.model.zed.zedNet import zedNet, zedNetDWSep, zedNetMod
 from seg.model.transformer.create_model import create_transformer, create_vit
 from seg.model.transformer.create_modelV2 import create_transformerV2
 from seg.model.Fusion.fuse import SimpleFusion
 from .fuse import CCMFusionModule, MiniEncoderFuse, MiniEncoderFuseDWSep, MiniEncoderFuseDWSepRFB
-
-basic_0 = 24
 
 class MultiLevelInputFusionNetwork(nn.Module):
     def __init__(
@@ -28,11 +27,31 @@ class MultiLevelInputFusionNetwork(nn.Module):
         big_trans_model_cfg,
         sml_trans_model_cfg,
         decoder_cfg,
-        trans_model_cfg,
+        trans_model_cfg_copy,
         num_output_trans_big=64,
         num_output_trans_sml=1,
+        basic_0=24,
     ):
         super(MultiLevelInputFusionNetwork, self).__init__()
+        cnn_model_cfg['basic_0'] = basic_0
+
+        big_trans_model_cfg['image_size'] = trans_model_cfg_copy['image_size']
+        big_trans_model_cfg["dropout"] = trans_model_cfg_copy['dropout']
+        big_trans_model_cfg["drop_path_rate"] = trans_model_cfg_copy['drop_path_rate']
+        big_trans_model_cfg['n_cls'] = trans_model_cfg_copy['n_cls']
+        big_trans_model_cfg['decoder'] = decoder_cfg
+        big_trans_model_cfg['num_output_trans'] = num_output_trans_big
+        decoder_cfg['name'] = 'linear' 
+
+        sml_trans_model_cfg['image_size'] = trans_model_cfg_copy['image_size']
+        sml_trans_model_cfg["dropout"] = trans_model_cfg_copy['dropout']
+        sml_trans_model_cfg["drop_path_rate"] = trans_model_cfg_copy['drop_path_rate']
+        sml_trans_model_cfg['n_cls'] = trans_model_cfg_copy['n_cls']
+        sml_trans_model_cfg['decoder'] = decoder_cfg
+        sml_trans_model_cfg['num_output_trans'] = num_output_trans_sml
+
+        # ---- entrance flow to network ----
+
         self.init_block = nn.Sequential(
             BNRconv3x3(in_planes=3, out_planes=basic_0, stride=1),
             BNRconv3x3(in_planes=basic_0, out_planes=basic_0, stride=1),
@@ -43,6 +62,15 @@ class MultiLevelInputFusionNetwork(nn.Module):
         self.sample2 = InputProjectionA(2)
 
         self.BR_1 = nn.Sequential(nn.BatchNorm2d(basic_0 + basic_0), nn.PReLU(basic_0 + basic_0))
+
+
+        big_trans_model_cfg['in_channels'] = basic_0 + basic_0 # this would have been 3 otherwise. but its not even set
+        sml_trans_model_cfg['in_channels'] = basic_0 + basic_0 # this would have been 3 otherwise. but its not even set
+        self.big_trans = create_transformerV5(big_trans_model_cfg, decoder='linear')
+        self.sml_trans = create_transformerV5(sml_trans_model_cfg, decoder='linear')
+        
+
+        exit(1)
 
 
     def forward(self, input):

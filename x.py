2c451f8ff49881763e52407727af08bea6f72f33
John Lewis <lewisj34_local@ce-yc-dlearn6.eng.umanitoba.ca>
2022-05-31 13:24:41 -0700

    gpu tests

--- x.py
+++ x.py
@@ -1,142 +1,95 @@
-import os 
-import shutil
-import click
+import os
+from datetime import datetime
+import argparse
+import torch.multiprocessing as mp
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+from apex.parallel import DistributedDataParallel as DDP
+from apex import amp
 
-from regex import F 
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 200, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(200),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(200, 400, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(400),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*400, num_classes)
 
-def moveImgs(
-    origin_dir: str,
-    target_dir: str,
-    prefix: str,
-    trainDataset: bool = False,
-):
-    """
-    moves pics from imgs and ann dir in a test dataset from its current loc
-    to the master train set 
-    dir should be a test dataset dir with and images and annotations directory 
-    """
-    origin_img_dir = origin_dir + '/images/'
-    origin_ann_dir = origin_dir + '/masks/'
-
-    target_img_dir = target_dir + '/images/'
-    target_ann_dir = target_dir + '/masks/'
-
-    if trainDataset: # just gotta put this logic in because TrainDataset has image and mask as the dir name 
-        origin_img_dir = origin_dir + '/image/'
-        origin_ann_dir = origin_dir + '/mask/'
-
-    for file in os.listdir(origin_img_dir):
-        path = origin_img_dir + file
-        # print(path)
-        assert os.path.isfile(path), f'{path} is not a file. Check.'
-
-        origin = path
-        target_name = prefix + os.path.basename(path)
-        target_path = target_img_dir + target_name
-
-        print(f'target: {target_path}')
-        shutil.copy(origin, target_path)
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
 
-    for file in os.listdir(origin_ann_dir):
-        path = origin_ann_dir + file
-        assert os.path.isfile(path), f'{path} is not a file. Check.'
-
-        origin = path
-        target_name = prefix + os.path.basename(path)
-        target_path = target_ann_dir + target_name
-
-        print(f'target: {target_path}')
-        shutil.copy(origin, target_path)
-
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    parser.add_argument('--epochs', default=2, type=int, metavar='N',
+                        help='number of total epochs to run')
+    args = parser.parse_args()
+    train(0, args)
     
+def train(gpu, args):
+    torch.manual_seed(0)
+    model = ConvNet()
+    torch.cuda.set_device(gpu)
+    model.cuda(gpu)
+    batch_size = 100
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(gpu)
+    optimizer = torch.optim.SGD(model.parameters(), 1e-4)
+    # Data loading code
+    train_dataset = torchvision.datasets.MNIST(root='./data',
+                                            train=True,
+                                            transform=transforms.ToTensor(),
+                                            download=True)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                            batch_size=batch_size,
+                                            shuffle=True,
+                                            num_workers=0,
+                                            pin_memory=True)
 
-@click.command(help='')
-@click.option('--origin_dir', type=str, default='/home/lewisj34_local/Dev/Datasets/master_polyp_mixed')
-@click.option('--target_dir', type=str, default='/home/lewisj34_local/Dev/Datasets/master_polyp_mixed_final')
-def main(
-    origin_dir,
-    target_dir
-):
-    if not os.path.isdir(target_dir):
-        print(f'target_dir: {target_dir} DNE. Creating...')
-        os.mkdir(target_dir)
-        if not os.path.isdir(target_dir + '/TestDataset/'):
-            os.mkdir(target_dir + '/TestDataset/')
-            os.mkdir(target_dir + '/TestDataset/images')
-            os.mkdir(target_dir + '/TestDataset/masks')
-        if not os.path.isdir(target_dir + '/TrainDataset/'):
-            os.mkdir(target_dir + '/TrainDataset/')
-            os.mkdir(target_dir + '/TrainDataset/images')
-            os.mkdir(target_dir + '/TrainDataset/masks')
-        if not os.path.isdir(target_dir + '/MergedDataset/'):
-            os.mkdir(target_dir + '/MergedDataset/')
-            os.mkdir(target_dir + '/MergedDataset/images')
-            os.mkdir(target_dir + '/MergedDataset/masks')
-    else:
-        if not os.path.isdir(target_dir + '/TestDataset/'):
-            os.mkdir(target_dir + '/TestDataset/')
-            os.mkdir(target_dir + '/TestDataset/images')
-            os.mkdir(target_dir + '/TestDataset/masks')
-        if not os.path.isdir(target_dir + '/TrainDataset/'):
-            os.mkdir(target_dir + '/TrainDataset/')
-            os.mkdir(target_dir + '/TrainDataset/images')
-            os.mkdir(target_dir + '/TrainDataset/masks')
-        if not os.path.isdir(target_dir + '/MergedDataset/'):
-            os.mkdir(target_dir + '/MergedDataset/')
-            os.mkdir(target_dir + '/MergedDataset/images')
-            os.mkdir(target_dir + '/MergedDataset/masks')
-        print(f'target_dir: {target_dir} and derivatives already exists.')
+    start = datetime.now()
+    total_step = len(train_loader)
+    for epoch in range(args.epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
 
-    # train dataset
-    train_dir = origin_dir + '/TrainDataset/'
-
-    # test datasets
-    cvc300_dir = origin_dir + '/TestDataset/CVC-300'
-    cvcClinic_dir = origin_dir + '/TestDataset/CVC-ClinicDB'
-    cvcColon_dir = origin_dir + '/TestDataset/CVC-ColonDB'
-    ETIS_dir = origin_dir + '/TestDataset/ETIS-LaribPolypDB'
-    kvasir_dir = origin_dir + '/TestDataset/Kvasir'
-
-    # home computer stuff 
-    # cvc300_dir = '/home/john/Documents/Datasets/master_polyp_mixed/TestDataset/CVC-300'
-    # cvcClinic_dir = '/home/john/Documents/Datasets/master_polyp_mixed/TestDataset/CVC-ClinicDB'
-    # cvcColon_dir = '/home/john/Documents/Datasets/master_polyp_mixed/TestDataset/CVC-ColonDB'
-    # ETIS_dir = '/home/john/Documents/Datasets/master_polyp_mixed/TestDataset/ETIS-LaribPolypDB'
-    # kvasir_dir = '/home/john/Documents/Datasets/master_polyp_mixed/TestDataset/Kvasir'
-
-    moveImgs(
-        cvc300_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'cvc_300_'
-    )
-    moveImgs(
-        cvcClinic_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'cvc_ClinicDB_'
-    )
-    moveImgs(
-        cvcColon_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'cvc_ColonDB_'
-    )
-    moveImgs(
-        ETIS_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'ETIS_'
-    )
-    moveImgs(
-        kvasir_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'kvasir_'
-    )
-    moveImgs(
-        train_dir,
-        target_dir + '/MergedDataset/',
-        prefix = 'train_',
-        trainDataset=True,
-    )
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i + 1) % 100 == 0 and gpu == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1, 
+                    args.epochs, 
+                    i + 1, 
+                    total_step,
+                    loss.item())
+                )
+    if gpu == 0:
+        print("Training complete in: " + str(datetime.now() - start))
 
 if __name__ == '__main__':
     main()
